2ebeb545b8b044db5e40846781defd5f702b22b6
Boitumeloh <163663595+Boitumeloh@users.noreply.github.com>
2024-10-15 17:20:02 +0200

    Delete BlogPostWork directory

--- BlogPostWork/controllers/pagesController.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    about: (req, res) => {
-        res.render('about'); //about.ejs
-    },
-    contact: (req, res) => {
-        res.render('contact'); //contact.ejs
-    }
-};
